a0ae3861afb8fcac94c8dc78d84c43a57dcabd39
Filip Smets <alemaaltevinden@gmail.com>
2014-10-04 11:50:45 +0200

    Made the php public

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+	$database_host = "localhost";
+	$database_user = "urba_logic";
+	$database_pass = "QoqmUR9V";
+	$database_name = "urba_logic";	
+	$database = new mysqli($database_host, $database_user, $database_pass, $database_name);
+	if ($database->connect_errno) {
+		echo "Failed to connect to database: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}	function jsonError ($error) {		exit('{"error": "' . $error . '"}');	}
+?>
